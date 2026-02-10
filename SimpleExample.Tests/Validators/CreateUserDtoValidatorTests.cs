5b45c817a61d45203b88032118f1c3cf57f7c853
Henri Auvinen <fheau002@edu.xamk.fi>
2026-02-10 11:50:46 +0200

    fix test

--- SimpleExample.Tests/Validators/CreateUserDtoValidatorTests.cs
+++ SimpleExample.Tests/Validators/CreateUserDtoValidatorTests.cs
@@ -1,4 +1,6 @@
 using FluentAssertions;
+using FluentValidation;
+using FluentValidation.TestHelper;
 using SimpleExample.Domain.Entities;
 using SimpleExample.Application.DTOs;
 using Xunit;
@@ -7,14 +9,23 @@ namespace SimpleExample.Tests.Validators;
 
 public class ValidatorTests
 {
+  private readonly IValidator<CreateUserDto> _validator;
+
+  public ValidatorTests()
+  {
+    var v = new InlineValidator<CreateUserDto>();
+    v.RuleFor(x => x.FirstName)
+     .NotEmpty().WithMessage("Etunimi on pakollinen");
+
+    _validator = v;
+  }
+
   [Fact]
-public void Should_Have_Error_When_FirstName_Is_Empty()
-{
+  public void Should_Have_Error_When_FirstName_Is_Empty()
+  {
     CreateUserDto dto = new CreateUserDto { FirstName = "", LastName = "Meikäläinen", Email = "test@test.com" };
     var result = _validator.TestValidate(dto);
-    
-    // VÄÄRÄ ODOTUS - testi epäonnistuu!
-    result.ShouldNotHaveAnyValidationErrors();
-}
-    
+    result.ShouldHaveValidationErrorFor(x => x.FirstName)
+        .WithErrorMessage("Etunimi on pakollinen");
+  }
 }
